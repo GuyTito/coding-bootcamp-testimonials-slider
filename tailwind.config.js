14654cd0ac9265c121bfa3d24a7b2f7a2eb7ef8f
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-02-16 12:44:46 +0000

    implemented style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./*.html",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Dark-Blue': 'hsl(240, 38%, 20%)',
+        'Grayish-Blue': 'hsl(240, 18%, 77%)',
+      },
+      fontFamily: {
+        sans : ["'Inter'", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
